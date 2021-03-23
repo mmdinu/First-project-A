db53b935ea247b32018d97bd6fb73cfb876a45b5
mmdinu <75750050+mmdinu@users.noreply.github.com>
2021-03-23 22:49:03 +0200

    Am adus si proiectul din 19 martie

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is my first java program");
+
+        System.out.println("I tray to print thou text");
+    }
+}
